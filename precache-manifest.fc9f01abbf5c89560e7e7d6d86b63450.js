42136f324d18559260be52c9fd04efba27cc72f0
Rodrigo Fraga <digo_via@hotmail.com>
2020-07-16 22:57:53 -0300

    site

---
+++ precache-manifest.fc9f01abbf5c89560e7e7d6d86b63450.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92eea5e6ff4727d57ee334fa0d7d6df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05520548e95316f761ba",
+    "url": "/static/css/main.5228d7b6.chunk.css"
+  },
+  {
+    "revision": "9856d39efd39db2dae39",
+    "url": "/static/js/2.b38ff932.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b38ff932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05520548e95316f761ba",
+    "url": "/static/js/main.93fd7fad.chunk.js"
+  },
+  {
+    "revision": "58c0c3759135ae774e3f",
+    "url": "/static/js/runtime-main.d60c4bef.js"
+  },
+  {
+    "revision": "270878767d03b78bd98f82a8df8fc649",
+    "url": "/static/media/LogoSVG.27087876.svg"
+  },
+  {
+    "revision": "3a7c47cbda46dffafac595162b85742f",
+    "url": "/static/media/Roboto-Regular-webfont.3a7c47cb.ttf"
+  },
+  {
+    "revision": "6de9e7e891343e0237c9b4a2c6eaab4d",
+    "url": "/static/media/Roboto-Regular-webfont.6de9e7e8.eot"
+  },
+  {
+    "revision": "b3be96cbbfd0ff1f3ba3c7c2f58c1441",
+    "url": "/static/media/Roboto-Regular-webfont.b3be96cb.woff"
+  },
+  {
+    "revision": "f5a9542491dc19cc3aff1c396698e9f7",
+    "url": "/static/media/Roboto-Regular-webfont.f5a95424.svg"
+  }
+]);
